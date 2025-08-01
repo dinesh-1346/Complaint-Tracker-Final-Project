b3e4b1dbc7a7fcf40236507be035257dfc73fd89
dinesh-1346 <andlurudineshreddy@gmail.com>
2025-08-01 16:17:28 +0530

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+// config/db.php
+$host = "localhost";
+$db = "complaint_db";
+$user = "root";
+$pass = "";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
