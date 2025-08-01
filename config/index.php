f186cffd9917f052d651cf69e8fd2b3760609b0f
dinesh-1346 <andlurudineshreddy@gmail.com>
2025-08-01 16:17:46 +0530

    Create index.php

---
+++ config/index.php
@@ -0,0 +1,10 @@
+<!-- index.php -->
+<?php include 'includes/header.php'; ?>
+<div class="container mt-5">
+  <h1 class="text-center">Welcome to the Digital Complaint Tracker</h1>
+  <div class="text-center mt-4">
+    <a href="pages/register.php" class="btn btn-primary">Register</a>
+    <a href="pages/login.php" class="btn btn-success">Login</a>
+  </div>
+</div>
+<?php include 'includes/footer.php'; ?>
