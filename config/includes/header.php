837effe65e9be4a471dd21aead04927b8e7a3c2f
dinesh-1346 <andlurudineshreddy@gmail.com>
2025-08-01 16:18:07 +0530

    Create header.php

---
+++ config/includes/header.php
@@ -0,0 +1,8 @@
+<!-- includes/header.php -->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Complaint Tracker</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
